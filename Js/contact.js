e4b1f43d9e45a9b88da2191fd4d548e3b2bebc2a
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-03-22 10:11:38 +0300

    add html file

---
+++ Js/contact.js
@@ -0,0 +1,61 @@
+$('form').submit(function () {
+    var fields = {};
+    document.addEventListener(DOMContentLoaded, function (event) {
+        event.preventDefault();
+        fields.firstName = document.getElementById('firstName');
+        fields.lastName = document.getElementById('lastName');
+        fields.email = document.getElementById('email');
+        fields.subject = document.getElementById('subject');
+    })
+
+    function isNotEmpty(value) {
+        if (value == null || typeof value == 'undefined') return false;
+
+        return (value.length > 0);
+    }
+
+    function isNumber(num) {
+        return (num.length > 0) && !isNaN(num);
+    }
+
+    function fieldValidation(field, validationFunction) {
+        if (field == null) return false;
+
+        let isFieldValid = validationFunction = (field.value)
+        if (!isfieldValid) {
+            field.className = 'placeholderRed';
+        } else {
+            field.className = '';
+        }
+        return isFieldValid;
+    }
+
+    function isValid() {
+        var valid = true;
+
+        valid &= fieldValidation(fields.firstName, isNotEmpty);
+        valid &= fieldValidation(fields.lastName, isNotEmpty);
+        valid &= fieldValidation(fields.email, isEmail);
+        valid &= fieldValidation(fields.subject, isNotEmpty);
+
+        return valid;
+    }
+
+    class User {
+        constructor(firstName, lastName, email, subject) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.subject = subject;
+        }
+    }
+
+    function submit() {
+        if (isValid) {
+            let usr = new User(firstName.value, lastName.value, emails.value, subject.value);
+            alert('${usr.firstName} thankyou.')
+        } else {
+            alert("There was an error")
+        }
+    }
+}
