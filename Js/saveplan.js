0e950195789c330a92492650375cdfee95ad397a
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-03-23 08:48:34 +0300

    rf:make changes on html css js files

--- Js/saveplan.js
+++ Js/saveplan.js
@@ -8,4 +8,10 @@ $("#travel").click (function (){
     $("#safaricom").toggle();
    });
    
-   
+   $("button#btn").click( () => {
+     alert("You card payment is currently being processed")
+   })
+
+   $("button#stk").click( () => {
+    alert("An STK popup will be sent to your phone shortly")
+  })
